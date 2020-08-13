2ced4befb64d693e67f3b6358f339d9db5b5ceea
devanrezqyp <69294999+devanrezqyp@users.noreply.github.com>
2020-08-13 22:42:35 +0700

    Add files via upload

---
+++ String Template.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val kotlin = """
+        Nama saya kotlin
+        Saya berusia 4 tahun
+        Saat ini saya sudah mencapai versi 1.3.70
+        
+    """.trimIndent()
+
+    print(kotlin)
+    
+}
